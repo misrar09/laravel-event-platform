d7e4f85225e3064228c4c47b8af3f1130935d8b4
israr <misrar09@gmail.com>
2024-02-08 10:34:53 +0100

    added user in EventController api

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -11,7 +11,7 @@ class EventController extends Controller
     public function index()
     {
 
-        $results = Event::all();
+        $results = Event::with('user')->get();
 
 
         $data = [
